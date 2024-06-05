45fe6c0a2bdcca852c987ba8d7d33dc17a77245f
Harshvardhan <shreeharshpb11@gmail.com>
2024-06-06 01:40:06 +0530

    add componen in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-
-
+import "./App.css";
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main>
+      <div className="main">
+        <div className="gradient"></div>
+      </div>
+      <div className="app">
+        <Hero/>
+        <Demo/>
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
