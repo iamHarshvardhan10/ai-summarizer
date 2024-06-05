d5d17a2a47b4e879a84dd0b43f130d8acd80ee2c
Harshvardhan <shreeharshpb11@gmail.com>
2024-06-06 01:53:18 +0530

    complete hero page Ui design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,30 @@
-
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 p-3">
+        <img src={logo} alt="sumz" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            window.open("https://github.com/iamharshvardhan10");
+          }}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source articel summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
